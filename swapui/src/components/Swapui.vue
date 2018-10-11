acc20d683b646c228dcf089b0ff680c4ddd789e9
rbantos <remibantos@remibantos.noreply.github.com>
2018-10-11 13:46:44 +0200

    Declarative rendering

--- swapui/src/components/Swapui.vue
+++ swapui/src/components/Swapui.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="swapui">
         <h1>{{msg}}</h1>
+        <h2>because we are {{date}}</h2>
     </div>
 </template>
 <script>
   export default {
     name: 'Swapui',
-    props: ['msg']
+    props: ['msg'],
+    data () {
+      return {
+        date: new Date()
+      }
+    }
   }
 </script>
 <style scoped>
